85ad4d110e2aaeb3fcfb7bb82c9f90137a52ffe8
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2024-10-22 08:38:56 -0700

    add specs to base class

--- bdr/bdr_tools/tools/base.py
+++ bdr/bdr_tools/tools/base.py
@@ -1,18 +1,24 @@
 from kubiya_sdk.tools import Tool
 
-GCP_ICON_URL = "https://cloud.google.com/_static/cloud/images/social-icon-google-cloud-1200-630.png"
+from tools.common import (COMMON_ENVIRONMENT_VARIABLES, 
+                          COMMON_FILE_SPECS)
+
+
+ICON_URL = "https://cloud.google.com/_static/cloud/images/social-icon-google-cloud-1200-630.png"
 
 class BDRTool(Tool):
     def __init__(self, name, description, content, args, long_running=False, mermaid_diagram=None):
         super().__init__(
             name=name,
             description=description,
-            icon_url=GCP_ICON_URL,
+            icon_url=ICON_URL,
             type="docker",
             image="python:3.11-bullseye",
             content=content,
             args=args,
             env=["GITHUB_TOKEN"],
+            with_files=COMMON_FILE_SPECS,
+            env=COMMON_ENVIRONMENT_VARIABLES,
             long_running=long_running,
             mermaid=mermaid_diagram
         )
--- bdr/bdr_tools/tools/bdr.py
+++ bdr/bdr_tools/tools/bdr.py
@@ -4,7 +4,7 @@ from tools.base import BDRTool, register_bdr_tool
 get_envs = BDRTool(
     name="get_envs",
     description="Get Environment Variables",
-    content="python /tmp/printenv.py",
+    content="python ./printenv.py",
     args=[],
     mermaid_diagram="..."  # Add mermaid diagram here
 )
@@ -12,7 +12,7 @@ get_envs = BDRTool(
 get_github_repo_commit_list = BDRTool(
     name="get_github_repo_commit_list",
     description="Retrieve all the recent code committers from a github repo using this url",
-    content="python gitusers.py --github_repo_url '$github_repo_url' ",
+    content="python ./gitusers.py --github_repo_url '$github_repo_url' ",
     args=[
         Arg(name="github_repo_url", type="str", description="Github repo endpoint", required=True),
     ],
